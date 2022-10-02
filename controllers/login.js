f303d5fd79a0b5716e00ab051f616e4f767aca55
Daniel Ramirez <danielsinsama@gmail.com>
2022-10-02 09:29:58 -0500

    init

---
+++ controllers/login.js
@@ -0,0 +1,30 @@
+const {SelectAllUsers} = require('../database/login')
+const {hash} = require('../utils/crypto')
+const loginController = async (req,res)=>{
+    const {body} = req
+    try {
+        const [user]= await SelectAllUsers(body.username)
+        const valid= await hash(body.password,user.password)
+        // console.log('user: ',user);
+
+        res.status(200).send(
+            {
+                ok: valid,
+                user:user.fullname
+            }
+        )      
+    } 
+    catch (error) {
+        res.status(400).send(
+            {
+                ok: false,
+                data:error.toString()
+            }
+        )        
+    }   
+
+}
+
+module.exports = {
+    loginController
+}
---
+++ utils/crypto.js
@@ -0,0 +1,6 @@
+const bcrypt = require('bcrypt')
+const hash = async (password, passworddb) => {
+    const valid = await bcrypt.compare(password,passworddb)
+    return valid
+}
+module.exports = { hash }
