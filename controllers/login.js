8d96abf0e7d15e32d43c1bc0b7e142f8eba4448a
Daniel Ramirez <danielsinsama@gmail.com>
2022-10-03 22:38:24 -0500

    mejorando entrega de datos en el controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,19 +1,19 @@
-const {selectAllUsers} = require('../services/users')
+const { selectUserByUsername } = require('../services/users')
 const { hash } = require("../utils/crypto");
 const loginController = async (req, res) => {
   const { body } = req;
   try {
-    const [user] = await selectAllUsers(body.username);
+    const [user] = await selectUserByUsername(body.username);
     const valid = await hash(body.password, user.password);
-    // console.log('user: ',user);
     res.status(200).send({
       ok: valid,
-      user: valid ? user.fullname: "Wrong username or password",
+      msg: valid ? "User accepted" : "Wrong username or password",
+      data: valid ? user.fullname : null
     });
   } catch (error) {
     res.status(400).send({
       ok: false,
-      data: error.toString(),
+      msg: error.toString(),
     });
   }
 };
