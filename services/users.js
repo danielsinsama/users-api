d851f73abc59148cb0ce635f8ba8fde92d8e1f3a
Daniel Ramirez <danielsinsama@gmail.com>
2022-10-03 22:39:01 -0500

    actualizando nombre de funcion: selectUserByUsername

--- services/users.js
+++ services/users.js
@@ -1,15 +1,12 @@
 const {pool} = require('../database/config')
-const selectAllUsers = (username) => {
+const selectUserByUsername = (username) => {
     return new Promise(async (resolve, reject) => {
         const sql = `SELECT * FROM Users WHERE username = "${username}"`;
-        // console.log(sql);
         pool.query(sql, (error, elements) => {
-            if (error) {
-                return reject(error);
-            }
+            if (error) return reject(error);
             return resolve(elements);
         });
     });
 };
 
-module.exports = { selectAllUsers };
+module.exports = { selectUserByUsername };
