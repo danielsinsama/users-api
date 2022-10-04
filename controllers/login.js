443927cd95d183f674399bf59cab7c9e39201c62
Daniel Ramirez <danielsinsama@gmail.com>
2022-10-04 09:30:12 -0500

    se incluye posibilidad de usuario no encontrado : controllers/login.js

--- controllers/login.js
+++ controllers/login.js
@@ -4,6 +4,12 @@ const loginController = async (req, res) => {
   const { body } = req;
   try {
     const [user] = await selectUserByUsername(body.username);
+    if(!user)
+      return res.status(400).json({
+        ok:false,
+        msg:"User not found",
+        data:null
+      })
     const valid = await hash(body.password, user.password);
     const resp = !valid ? res.status(401) : res.status(200)
     return resp.send({
@@ -15,6 +21,7 @@ const loginController = async (req, res) => {
     return res.status(400).send({
       ok: false,
       msg: error.toString(),
+      data:null
     });
   }
 };
