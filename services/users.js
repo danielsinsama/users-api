f49f6b675fc479f75d40744a9db350784223291d
Daniel Ramirez <danielsinsama@gmail.com>
2022-10-03 22:48:15 -0500

    reduciendo async innecesario: services/users

--- services/users.js
+++ services/users.js
@@ -1,6 +1,6 @@
 const {pool} = require('../database/config')
 const selectUserByUsername = (username) => {
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         const sql = `SELECT * FROM Users WHERE username = "${username}"`;
         pool.query(sql, (error, elements) => {
             if (error) return reject(error);
